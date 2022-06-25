ab7dd4669a221dd2a74b11fc4d9aa259ed4bd01b
omkar-9905 <51113480+omkar-9905@users.noreply.github.com>
2022-06-25 15:37:27 -0700

    AddBinary-leetcode
    Link: https://leetcode.com/problems/add-binary/
    This is basic solution code can be more optimized

--- addBinary.py
+++ addBinary.py
@@ -1,1 +1,17 @@
-
+def stringToList(ans):
+ return [char for char in reversed(ans)]
+binary_a = stringToList(a)
+binary_b = stringToList(b)
+def toNum(num):
+    sum_a = []
+    for i,j in enumerate(num):
+        if j == "1":
+            sum_a.append(2**i)
+    return sum(sum_a)
+num_a = toNum(binary_a)
+num_b = toNum(binary_b)
+final_num_sum = num_a + num_b
+def decimalToBinary(n):
+    return "{0:b}".format(int(n))
+s = decimalToBinary(final_num_sum)
+return s
